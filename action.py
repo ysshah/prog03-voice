94115f48e9e4ba5a7147849cf83600f1979a62d2
tkrisivar <tkrisivar@gmail.com>
2017-03-13 20:39:24 -0700

    full implementation except exit intents
    Everything works except for the utterances "exit" and "quit" ("main menu" works"). Alexa is taking exit and quit as Session ending requests. Went to OH on Monday 03/13/17 but could not get it fixed. Will go to OH tomorrow.

--- action.py
+++ action.py
@@ -25,7 +25,31 @@ def on_intent(intent_request, session):
     # if "attributes" in session and session["attributes"].get("example", False):
     #     pass
 
-    if intent_name == "FindIntent":
+    if ("attributes" in session) and ("recipe" in session["attributes"]):
+        # we are in recipe and we will look for start ingredients list
+        # or start recipe and anything that is related to those two.
+        if "inIngred" in session["attributes"]:
+            if intent_name == "PrevIngredIntent":
+                return get_prev_ingred(intent, session)
+            elif intent_name == "NextIngredIntent":
+                return get_next_ingred(intent, session)
+            elif intent_name == "RestartIntent":
+                return start_ingred(intent, session)
+        elif "inInst" in session["attributes"]:
+            if intent_name == "PrevInstIntent":
+                return get_prev_inst(intent, session)
+            elif intent_name == "NextInstIntent":
+                return get_next_inst(intent, session)
+            elif intent_name == "RestartIntent":
+                return start_inst(intent, session)
+        elif intent_name == "HomeIntent":
+            return go_to_home(intent, session)
+        elif intent_name == "StartIngredIntent":
+            return start_ingred(intent, session)
+        elif intent_name == "StartInstIntent":
+            return start_inst(intent, session)
+
+    elif intent_name == "FindIntent":
         return find_recipe(intent)
 
     elif intent_name == "AMAZON.CancelIntent":
@@ -39,7 +63,7 @@ def on_intent(intent_request, session):
 
 def find_recipe(intent):
     recipes = json.loads(urllib2.urlopen("https://ffgh18ctp9.execute-api."
-        "us-east-1.amazonaws.com/prod/RecipeUpdate?TableName=Recipes").read())
+                                         "us-east-1.amazonaws.com/prod/RecipeUpdate?TableName=Recipes").read())
     query = intent["slots"]["SearchTerms"]["value"].lower()
     for recipe in recipes["Items"]:
         if recipe["RecipeName"].lower() == query:
@@ -55,19 +79,134 @@ def begin_recipe(recipe):
     speech_output = "I found your recipe for {}.".format(recipe["RecipeName"])
     session_attributes = {"recipe": recipe}
     return build_response(build_speechlet_response(speech_output, card_title),
-        session_attributes)
+                          session_attributes)
+
+
+def start_ingred(intent, session):
+    card_title = "First Ingredient"
+    ingred_start = 0
+    ingred_end = 0
+    recipe = session["attributes"]["recipe"]
+    ingredients = recipe["Ingredients"]
+    while ingredients[ingred_end] != "\n":
+        ingred_end += 1
+    first_ingred = ingredients[ingred_start: ingred_end]  # ingred_end is the \n character
+    speech_output = "the first ingredient is " + first_ingred
+    session_attributes = {"recipe": recipe, "ingred_start": ingred_start, "ingred_end": ingred_end, "inIngred": 0}
+    return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+
+def start_inst(intent, session):
+    card_title = "First Instruction"
+    inst_start = 0
+    inst_end = 0
+    recipe = session["attributes"]["recipe"]
+    instructions = recipe["Directions"]
+    while instructions[inst_end] != "\n":
+        inst_end += 1
+    first_inst = instructions[inst_start: inst_end]
+    speech_output = first_inst
+    session_attributes = {"recipe": recipe, "inst_start": inst_start, "inst_end": inst_end, "inInst": 0}
+    return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+
+def get_next_ingred(intent, session):
+    ingred_start = session["attributes"]["ingred_start"]
+    ingred_end = session["attributes"]["ingred_end"]
+    recipe = session["attributes"]["recipe"]
+    ingredients = recipe["Ingredients"]
+    ingred_end = ingred_end + 1
+    ingred_start = ingred_end  # skip over \n character
+    while ((ingred_end + 1) != len(ingredients)) and (ingredients[ingred_end] != "\n"):
+        ingred_end += 1
+    next_ingred = ingredients[ingred_start: ingred_end]
+
+    if (ingred_end + 1) == len(ingredients):
+        card_title = "Final Ingredient"
+        speech_output = "the final ingredient is " + next_ingred
+        session_attributes = {"recipe": recipe}
+        return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+    card_title = "Next Ingredient"
+    speech_output = next_ingred
+    session_attributes = {"recipe": recipe, "ingred_start": ingred_start, "ingred_end": ingred_end, "inIngred": 0}
+    return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+
+def get_next_inst(intent, session):
+    inst_start = session["attributes"]["inst_start"]
+    inst_end = session["attributes"]["inst_end"]
+    recipe = session["attributes"]["recipe"]
+    instructions = recipe["Directions"]
+    inst_end = inst_end + 1
+    inst_start = inst_end
+    while ((inst_end + 1) != len(instructions)) and (instructions[inst_end] != "\n"):
+        inst_end += 1
+    next_inst = instructions[inst_start: inst_end]
+
+    if (inst_end + 1) == len(instructions):
+        card_title = "Last Instruction"
+        speech_output = "last, " + next_inst
+        session_attributes = {"recipe": recipe}
+        return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+    card_title = "Next Instruction"
+    speech_output = next_inst
+    session_attributes = {"recipe": recipe, "inst_start": inst_start, "inst_end": inst_end, "inInst": 0}
+    return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+
+def get_prev_ingred(intent, session):
+    card_title = "Prev Ingredient"
+    ingred_start = session["attributes"]["ingred_start"]
+    ingred_end = session["attributes"]["ingred_end"]
+    recipe = session["attributes"]["recipe"]
+    ingredients = recipe["Ingredients"]
+    ingred_end = ingred_start - 1
+    ingred_start = ingred_start - 2
+    while ingred_start != 0 and ingredients[ingred_start] != "\n":
+        ingred_start -= 1
+    if ingredients[ingred_start] == "\n":
+        ingred_start += 1
+    prev_ingred = ingredients[ingred_start: ingred_end]
+    speech_output = prev_ingred
+    session_attributes = {"recipe": recipe, "ingred_start": ingred_start, "ingred_end": ingred_end, "inIngred": 0}
+    return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+
+def get_prev_inst(intent, session):
+    card_title = "Prev Instruction"
+    inst_start = session["attributes"]["inst_start"]
+    inst_end = session["attributes"]["inst_end"]
+    recipe = session["attributes"]["recipe"]
+    instructions = recipe["Directions"]
+    inst_end = inst_start - 1
+    inst_start -= 2
+    while inst_start != 0 and instructions[inst_start] != "\n":
+        inst_start -= 1
+    if instructions[inst_start] == "\n":
+        ingred_start += 1
+    prev_inst = instructions[inst_start: inst_end]
+    speech_output = prev_ints
+    session_attributes = {"recipe": recipe, "inst_start": inst_start, "inst_end": inst_end, "inInst": 0}
+    return build_response(build_speechlet_response(speech_output, card_title), session_attributes)
+
+
+def go_to_home(launch_request, session):
+    card_title = "Back to home"
+    speech_output = "What recipe would you like to make?"
+    return build_response(build_speechlet_response(speech_output, card_title))
 
 
 def handle_session_end_request():
     card_title = "Thank you!"
     speech_output = "Thank you for using Recipe Assistant."
     return build_response(build_speechlet_response(speech_output, card_title,
-        should_end_session=True))
+                                                   should_end_session=True))
 
 
 def build_speechlet_response(speech_output, card_title=None,
-    card_output=None, reprompt_text=None, should_end_session=False):
-
+                             card_output=None, reprompt_text=None, should_end_session=False):
     if card_output is None:
         card_output = speech_output
 
